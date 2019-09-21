df55c2ec4e279b3cd8146d21fcce4b693e14e890
Edgar Mac <edbarrios5@hotmail.com>
2019-09-21 05:42:49 -0500

    Agrege Google Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,17 @@ module.exports = {
             },
           },
           {
+            resolve: `gatsby-plugin-gtag`,
+            options: {
+              // your google analytics tracking id
+              trackingId: `UA-102311-64`,
+              // Puts tracking script in the head instead of the body
+              head: false,
+              // enable ip anonymization
+              anonymize: true,
+            },
+          },
+          {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
               destinationDir: 'static',
